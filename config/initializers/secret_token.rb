0d8d959c97d9827bbe5a63a58bd42c566223b586
Jen <genielizj@hotmail.com>
2013-11-01 18:33:18 -0700

    Proyecto inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JENNY::Application.config.secret_token = 'fd0836c64786a2ef35d60cbe5c0a0f3cae479d6c8b647a6a20b2208942f1dcf0834cddd7c9555600e31c8af7870b198efd2579d2588df726261b5ae7671cd61d'
